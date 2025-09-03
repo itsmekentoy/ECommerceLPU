4c681f97300ecf120b4215fe2c5b1f9b2a868c31
itsmekentoy <kent.troubleshooter@gmail.com>
2025-09-03 21:54:55 +0800

    add poppins as main font

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -8,6 +8,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=poppins:100i,300,400,500,600,700,800,900" rel="stylesheet" />
+    <style>
+        *{
+            font-family: 'Poppins';
+        }
+    </style>
 </head>
 <body class="bg-gray-50 min-h-screen">
     <div class="h-screen p-4 flex flex-col">
